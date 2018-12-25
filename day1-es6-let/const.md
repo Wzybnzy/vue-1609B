f25452647c4f8bb3ac1bd7f9c2d27bfde4e5b314
yihang <1341674619@qq.com>
2018-12-25 09:41:19 +0800

    day1

---
+++ day1-es6-let/const.md
@@ -0,0 +1,18 @@
+### javascript 
+> DOM(Document Object Modal) + BOM(Browser Object Modal) + ECMAscript(核心语法标准)
+### es6
+
+2015年六月出版的第6版语法标准 简称es6
+
+### 声明变量
+
+> const
+
+1. 声明常量（不能再次修改值，引用类型不能修改地址）
+2. {}会产生块级作用域 
+    es5之前只有函数作用域 
+3. 不会变量提升
+4. const 不能在声明时不赋值
+5. const 声明不能重名
+
+
---
+++ day1-es6-let/index.js
@@ -0,0 +1,43 @@
+//  javascript =  DOM + BOM + ECMAscript(核心)
+
+// es6 
+
+
+//var function 
+
+//let const 
+
+// var a = function(){
+//     console.log('1')
+// }
+
+// function a(){
+//     console.log('2')
+// }
+
+// a();
+
+//const 常量 
+// const obj = {
+//     person:"zs"
+// };
+
+// obj = {};
+
+// console.log(obj);
+
+
+const str = 'bawei';
+if(10>5){
+    const str = 'zs';
+    console.log(str);
+}
+
+// const str;
+// if(10>5){
+//     str = 'bawei';
+    
+// }
+// console.log(str);
+
+
